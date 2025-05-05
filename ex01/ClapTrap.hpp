a7b528a5e7ee8e980e3517e8bd72f74dc94992ec
Martin Georges <martingeorges@MacBook-Air-de-Martin.local>
2025-05-05 15:56:29 +0200

    added ex01

---
+++ ex01/ClapTrap.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgeorges <mgeorges@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 11:17:03 by mgeorges          #+#    #+#             */
+/*   Updated: 2025/05/05 13:10:39 by mgeorges         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+private:
+    std::string _name;
+    unsigned int _hit_points;
+    unsigned int _energy_points;
+    unsigned int _attack_damage;
+public:
+    //Canonical form
+    ClapTrap();
+    ClapTrap(const ClapTrap &copy);
+    ClapTrap(std::string name);
+    ~ClapTrap();
+    ClapTrap & operator = (const ClapTrap &other);
+
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+};
+
+
+#endif 
---
+++ ex01/ScavTrap.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgeorges <mgeorges@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 15:40:14 by mgeorges          #+#    #+#             */
+/*   Updated: 2025/05/05 15:50:49 by mgeorges         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include <iostream>
+
+class ScavTrap
+{
+private:
+    /* data */
+public:
+    ScavTrap(/* args */);
+    ~ScavTrap();
+};
+
+#endif
